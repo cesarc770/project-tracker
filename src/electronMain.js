5ab4fdd9fc4b16f47cd40ab068d8e71887c86444
Cesar Caceres <cesarc770@gmail.com>
2018-06-19 22:48:32 -0600

    initial commit

---
+++ src/electronMain.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
+
+function isDev() {
+    return process.mainModule.filename.indexOf('app.asar') === -1;
+}
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 1200,
+        height: 800
+    })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../dist/project-tracker/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    win.on('closed', () => {
+        win = null;
+    })
+}
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+})
